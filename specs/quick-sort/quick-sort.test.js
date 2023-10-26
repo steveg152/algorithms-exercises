9ce4fbeafad01a7a61f4614e1eab471f576976e6
steveg152 <steve.greco2@gmail.com>
2023-10-26 17:28:01 -0400

    Quick Sort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -14,11 +14,26 @@
 
 function quickSort(nums) {
   // code goes here
+  if (nums.length <= 1) return nums;
+
+  const pivot = nums[nums.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
